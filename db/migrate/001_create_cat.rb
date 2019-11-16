bf52ad312dcb57f9be7d726aada212b344879f47
rnovick13 <rnovick13@gmail.com>
2019-11-15 23:50:27 -0500

    migration

---
+++ db/migrate/001_create_cat.rb
@@ -0,0 +1,19 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cats do |t|
+        t.string "name"
+        t.string "favorite_food"
+        t.integer "age"
+        t.integer "person_id"
+        t.timestamps
+      end
+    end
+end
+
+create_table :cats do |t|
+      t.string "name"
+      t.string "favorite_food"
+      t.integer "age"
+      t.integer "person_id"
+      t.timestamps
+    end
---
+++ db/migrate/002_create_people.rb
@@ -0,0 +1,9 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+      create_table :people do |t|
+        t.string "name"
+        t.integer "age"
+        t.timestamps
+      end
+    end
+end
