e3784ac5c3d3642d842b82bcc30fbf29c70d4b43
rnovick13 <rnovick13@gmail.com>
2019-11-15 23:52:04 -0500

    removed extra code

--- db/migrate/001_create_cat.rb
+++ db/migrate/001_create_cat.rb
@@ -9,11 +9,3 @@ class CreateCats < ActiveRecord::Migration[5.2]
       end
     end
 end
-
-create_table :cats do |t|
-      t.string "name"
-      t.string "favorite_food"
-      t.integer "age"
-      t.integer "person_id"
-      t.timestamps
-    end
