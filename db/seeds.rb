e3bec797b8b38d03088cc483ff42c9e9f10cc9d6
rnovick13 <rnovick13@gmail.com>
2019-11-15 23:55:57 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Person.destroy_all
+Cat.destroy_all
+
+phil = Person.create({name: "Phil", age: 23})
+alfred = Person.create({name: "Alfred", age: 25})
+phil.cats.create({name: "Fluffles", age: 3, favorite_food: "friskies"})
+phil.cats.create({name: "Spot", age: 11, favorite_food: "salmon"})
+phil.cats.create({name: "Furtha", age: 1, favorite_food: "chicken"})
+alfred.cats.create({name: "Meowserino", age: 5, favorite_food: "Garbanzo Beans"})
